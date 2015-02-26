5137d23eb32ffdaa54596ed3162d24985c394866
fbarroso24 <fbarroso24@users.noreply.github.com>
2015-02-26 00:33:24 -0500

    Implmenet init() function inside game.js in order to draw google blue board and ms pacman on top

--- mspacman/game.js
+++ mspacman/game.js
@@ -1,1 +1,11 @@
-// Replace this with your code...
+function init() {
+	var ctx = document.getElementById('game_canvas').getContext('2d');
+	var img= new Image();
+	img.src = 'pacman10-hp-sprite.png';
+
+	// Draw Google background
+	ctx.drawImage(img, 320, 0, 466, 138, 0, 0, 466, 138);
+
+	// Draw Ms. Pacman on top of background
+	ctx.drawImage(img, 80, 24, 20, 15, 37, 32, 18, 16);
+}
