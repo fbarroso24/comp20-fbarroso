eedb02ed5fcda9e489999629116bb6bf41cee1c5
fbarroso24 <fbarroso24@users.noreply.github.com>
2015-02-26 22:42:40 -0500

    add onload function to image to ensure we only draw on the canvas once the image has finished loading

--- mspacman/game.js
+++ mspacman/game.js
@@ -2,10 +2,11 @@ function init() {
 	var ctx = document.getElementById('game_canvas').getContext('2d');
 	var img= new Image();
 	img.src = 'pacman10-hp-sprite.png';
+	img.onload = function() {
+		// Draw Google background
+		ctx.drawImage(img, 320, 0, 466, 138, 0, 0, 466, 138);
 
-	// Draw Google background
-	ctx.drawImage(img, 320, 0, 466, 138, 0, 0, 466, 138);
-
-	// Draw Ms. Pacman on top of background
-	ctx.drawImage(img, 80, 24, 20, 15, 37, 32, 18, 16);
+		// Draw Ms. Pacman on top of background
+		ctx.drawImage(img, 80, 24, 20, 15, 37, 32, 18, 16);
+	}
 }
